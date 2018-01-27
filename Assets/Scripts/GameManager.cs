771e645a2b962f1af634ba454a2f405a42bc4bbb
TEMPa Dzida <whiteada@gmail.com>
2018-01-28 00:58:43 +0100

    Jeszcze nieprzetestowane ustawione logiczne poruszanie po nodach, main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour {
+
+    public Board mainBoard;
+    public int allCards;
+    public int playerActiveId;
+    public List<PlayerScript> playersList;
+    public Transform[] obelisks;
+    public GameObject[] wikiPonPrefabs;
+
+
+	// Use this for initialization
+	void Start () {
+        InitData();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    internal void InitData()
+    {
+        allCards = 72;
+
+        for (int i = 0; i < obelisks.Length; i++)
+        {
+            obelisks[i].DOKill();
+            obelisks[i].transform.position = mainBoard.startPlayerNodes[i].position;
+        }
+    }
+
+    internal void RemoveCard(int countCard)
+    {
+        allCards -= countCard;
+
+        if (allCards <= 0)
+        {
+            EndThisGame();
+        }
+    }
+
+    internal void EndThisGame()
+    {
+
+    }
+
+    public void CreateWikiPon(PlayerScript.WikiPonType wikiPonT)
+    {
+        if (wikiPonT == PlayerScript.WikiPonType.Blue)
+        {
+
+        }
+        else if (wikiPonT == PlayerScript.WikiPonType.Purple)
+        {
+
+        }
+        else if (wikiPonT == PlayerScript.WikiPonType.Red)
+        {
+
+        }
+        else if (wikiPonT == PlayerScript.WikiPonType.Yellow)
+        {
+
+        }
+    }
+}
