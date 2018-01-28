a54c79012189ff6a5989e27a364b5f2e9b402c08
WisniaCFC <adam.wisniewski93@gmail.com>
2018-01-28 14:28:34 +0100

    Dodane dźwięki na wykrycie kompletu 3 kart

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public AudioClip clickClip;
+	public AudioClip endRoundClip;
+	public AudioClip startRoundClip;
+	public AudioClip pointsForEndClip;
+	public AudioClip pointsForObeliskClip;
+	public AudioClip redClip;
+	public AudioClip yellowClip;
+	public AudioClip blueClip;
+	public AudioClip purpleClip;
+
+	AudioSource source;
+
+	void Start ()
+	{
+		source = GetComponent<AudioSource>();
+	}
+	
+	public void PlaySound(string type)
+	{
+		if(type == "click")
+		{
+			source.clip = clickClip;
+		}
+		else if(type == "endRound")
+		{
+			source.clip = endRoundClip;
+		}
+		else if(type == "startRound")
+		{
+			source.clip = startRoundClip;
+		}
+		else if(type == "pointsEnd")
+		{
+			source.clip = pointsForEndClip;
+		}
+		else if(type == "pointsObelisk")
+		{
+			source.clip = pointsForObeliskClip;
+		}
+		else if(type == "red")
+		{
+			source.clip = redClip;
+		}
+		else if (type == "blue")
+		{
+			source.clip = blueClip;
+		}
+		else if (type == "yellow")
+		{
+			source.clip = yellowClip;
+		}
+		else if (type == "purple")
+		{
+			source.clip = purpleClip;
+		}
+
+		source.Play();
+	}
+}
