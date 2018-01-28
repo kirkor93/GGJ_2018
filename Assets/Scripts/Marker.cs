8b09a0949e690ad7c5b5c87f2eaebccebfc2a0e9
WisniaCFC <adam.wisniewski93@gmail.com>
2018-01-28 02:57:59 +0100

    Marker Manager, markery, rozpoznawanie 3 różnych symboli

---
+++ Assets/Scripts/Marker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marker : MonoBehaviour
+{
+	public PlayerScript.WikiPonType wikiType;
+	public PlayerScript.MoveDirection dir;
+	public int power;
+
+	public Type myType;
+
+	public enum Type
+	{
+		vikipod,
+		direction,
+		incrementation
+	}
+
+	public bool isShowed = false;
+
+	private void Update()
+	{
+		if (!isShowed && transform.GetComponentInChildren<MeshRenderer>().enabled)
+		{
+			isShowed = true;
+		}
+		else if(isShowed && !transform.GetComponentInChildren<MeshRenderer>().enabled)
+		{
+			isShowed = false;
+		}
+	}
+}
